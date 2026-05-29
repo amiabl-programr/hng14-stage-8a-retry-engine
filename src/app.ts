ef086b1beeb907672e5f035173a635273822ed12
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-05-29 14:24:23 +0100

    feat: add Pino logger with HTTP request logging

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { type Express } from "express";
+import { pinoHttp } from "pino-http";
+import logger from "./config/logger.js";
 
 const app: Express = express();
 
+app.use(pinoHttp({ logger }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app.js";
+import logger from "./config/logger.js";
 
 const PORT = process.env["PORT"] ?? 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  logger.info({ port: PORT }, `Server running on http://localhost:${PORT}`);
 });
